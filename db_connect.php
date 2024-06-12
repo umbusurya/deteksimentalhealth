efcfdce564048475711dc2afc5a5417860f6919c
umbusurya <126148188+umbusurya@users.noreply.github.com>
2024-06-12 12:18:13 +0700

    Add files via upload

--- db_connect.php
+++ db_connect.php
@@ -1,1 +1,14 @@
+<?php
+$servername = "localhost";
+$dbusername = "id22306897_root";
+$dbpassword = "Umbusurya_09";
+$dbname = "id22306897_webuas";
 
+// Create connection
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
